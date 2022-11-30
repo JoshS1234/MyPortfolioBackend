f7725a164c6cdc52ebb36e2f08e47c1e40372ba7
JOSH SPENCE <joshua.spence@hotmail.co.uk>
2022-11-30 13:35:16 +0000

    removed old seed files

--- Old/javascript.js
+++
@@ -1,8 +0,0 @@
-module.exports = [
-  {
-    name: "NC Games",
-    image: "available",
-    description:
-      "This is a forum style webpage which allows users to make reviews for various games",
-  },
-];
--- Old/python.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-  {
-    name: "Fractal pattern",
-    image: "available",
-    description: "This is a mathematical model which creates a fractal pattern",
-  },
-];
