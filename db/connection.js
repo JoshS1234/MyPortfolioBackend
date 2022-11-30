5b46c2608cf8028e8de1ac2980c3afe17a9e8e62
JOSH SPENCE <joshua.spence@hotmail.co.uk>
2022-11-30 15:23:26 +0000

    removed old code from seed file

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,8 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
-const ENV = process.env.NODE_ENV || 'development';
+const ENV = process.env.NODE_ENV || "development";
 const config =
-  ENV === 'production'
+  ENV === "production"
     ? {
         connectionString: process.env.DATABASE_URL,
         ssl: {
@@ -11,15 +11,15 @@ const config =
       }
     : {};
 
-require('dotenv').config({
+require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
 // if (!process.env.PGDATABASE) {
-//   throw new Error('PGDATABASE not set');
+//   throw new Error("PGDATABASE not set");
 // }
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-  throw new Error("PGDATABASE or DATABASE_URL not set")
+  throw new Error("PGDATABASE or DATABASE_URL not set");
 }
 
 module.exports = new Pool(config);
--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -9,34 +9,7 @@ const {
 const seed = async (data) => {
   const { pythonProjects, javascriptProjects, matlabProjects, allProjects } =
     data;
-  // await db.query(`DROP TABLE IF EXISTS pythonProjects;`);
-  // await db.query(`DROP TABLE IF EXISTS javascriptProjects;`);
-  // await db.query(`DROP TABLE IF EXISTS matlabProjects;`);
   await db.query(`DROP TABLE IF EXISTS projects;`);
-
-  // const pythonTablePromise = db.query(`
-  // CREATE TABLE pythonProjects (
-  //   project_id SERIAL PRIMARY KEY,
-  //   name VARCHAR,
-  // image VARCHAR,
-  //   description VARCHAR
-  // );`);
-
-  // const javascriptTablePromise = db.query(`
-  // CREATE TABLE javascriptProjects (
-  //   project_id SERIAL PRIMARY KEY,
-  //   name VARCHAR,
-  // image VARCHAR,
-  //   description VARCHAR
-  // );`);
-
-  // const matlabTablePromise = db.query(`
-  // CREATE TABLE matlabProjects (
-  //   project_id SERIAL PRIMARY KEY,
-  //   name VARCHAR,
-  // image VARCHAR,
-  //   description VARCHAR
-  // );`);
 
   const allProjectsTablePromise = db.query(`
   CREATE TABLE projects (
@@ -47,51 +20,7 @@ const seed = async (data) => {
     description VARCHAR
   );`);
 
-  await Promise.all([
-    // pythonTablePromise,
-    // javascriptTablePromise,
-    // matlabTablePromise,
-    allProjectsTablePromise,
-  ]);
-
-  // const insertMatlabProjectsQueryStr = format(
-  //   "INSERT INTO matlabProjects (name, image, description) VALUES %L RETURNING *;",
-  //   matlabProjects.map(({ name, image, description }) => [
-  //     name,
-  //     image,
-  //     description,
-  //   ])
-  // );
-
-  // const matlabPromise = db
-  //   .query(insertMatlabProjectsQueryStr)
-  //   .then((result) => result.rows);
-
-  // const insertJavascriptProjectsQueryStr = format(
-  //   "INSERT INTO javascriptProjects (name, image, description) VALUES %L RETURNING *;",
-  //   javascriptProjects.map(({ name, image, description }) => [
-  //     name,
-  //     image,
-  //     description,
-  //   ])
-  // );
-
-  // const javascriptPromise = db
-  //   .query(insertJavascriptProjectsQueryStr)
-  //   .then((result) => result.rows);
-
-  // const insertPythonProjectsQueryStr = format(
-  //   "INSERT INTO pythonProjects (name, image, description) VALUES %L RETURNING *;",
-  //   pythonProjects.map(({ name, image, description }) => [
-  //     name,
-  //     image,
-  //     description,
-  //   ])
-  // );
-
-  // const pythonPromise = db
-  //   .query(insertPythonProjectsQueryStr)
-  //   .then((result) => result.rows);
+  await Promise.all([allProjectsTablePromise]);
 
   const insertAllProjectsQueryStr = format(
     "INSERT INTO projects (name, image, language, description) VALUES %L RETURNING *;",
@@ -107,12 +36,7 @@ const seed = async (data) => {
     .query(insertAllProjectsQueryStr)
     .then((result) => result.rows);
 
-  await Promise.all([
-    // javascriptPromise,
-    // matlabPromise,
-    // pythonPromise,
-    allPromise,
-  ]);
+  await Promise.all([allPromise]);
 };
 
 module.exports = seed;
