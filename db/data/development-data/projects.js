85216c5d50c87bfbd0a8460cf418dc63a1e1be19
JOSH SPENCE <joshua.spence@hotmail.co.uk>
2022-12-04 23:40:36 +0000

    updated the database with links to github, images and videos

--- db/data/development-data/projects.js
+++ db/data/development-data/projects.js
@@ -3,8 +3,8 @@ module.exports = [
     name: "NC Games - Backend",
     image_url: "",
     video_url: "",
-    github: "https://github.com/JoshS1234/nc-games",
-    hosted: "https://sunny-narwhal-f4e1dd.netlify.app/",
+    github: "https://github.com/JoshS1234/be-nc-games",
+    hosted: "https://my-backend-server-josh.herokuapp.com/",
     language: "Javascript",
     tech: "Express",
     description:
@@ -13,24 +13,26 @@ module.exports = [
   },
   {
     name: "NC Games - Frontend",
-    image_url: "",
+    image_url:
+      "https://drive.google.com/file/d/1qRfdbcVJPwDZkVr4o6fCJyLdHlv16E_7/view?usp=share_link",
     video_url: "",
-    github: "",
-    hosted: "",
+    github: "https://github.com/JoshS1234/nc-games",
+    hosted: "https://sunny-narwhal-f4e1dd.netlify.app/",
     language: "Javascript",
-    tech: "react",
+    tech: "React",
     description:
       "This is the frontend for a (mock) board games review site, it was made using React and uses the 'NC Games - Backend' project to provide the data to populate the site. Users can also upvote and post comments",
     year: 2022,
   },
   {
     name: "Pokeguessr",
-    image_url: "",
+    image_url:
+      "https://drive.google.com/file/d/1dWP8pf7kNrfISw44gjH4BwT_myYrAN12/view?usp=share_link",
     video_url: "",
     github: "https://github.com/JoshS1234/PokemonGuessingGame",
     hosted: "https://candid-pasca-16abe5.netlify.app/",
     language: "Javascript",
-    tech: "react",
+    tech: "React",
     description:
       "This is a react webpage I created as a pokemon guessing game, it uses a pokemon data API (https://pokeapi.co/) to get images, and the user can filter by generation",
     year: 2022,
@@ -38,7 +40,7 @@ module.exports = [
   {
     name: "Snap App",
     image_url: "",
-    video_url: "",
+    video_url: "g47hsMFTKLA",
     github: "https://github.com/JoshS1234/MyUpdatedSnap",
     hosted: "",
     language: "Javascript",
@@ -50,7 +52,7 @@ module.exports = [
   {
     name: "Vicsek model of bird flocking",
     image_url: "",
-    video_url: "",
+    video_url: "kT1rA1vLfCs",
     github: "",
     hosted: "",
     language: "MATLAB",
@@ -62,8 +64,8 @@ module.exports = [
   {
     name: "Mandelbrot fractal pattern - Changing power",
     image_url: "",
-    video_url: "",
-    github: "",
+    video_url: "sp-zj7p8zPE",
+    github: "https://github.com/JoshS1234/MATLAB-Fractals-More-Complex",
     hosted: "",
     language: "MATLAB",
     tech: "",
@@ -73,9 +75,10 @@ module.exports = [
   },
   {
     name: "2D pendulum motion",
-    image_url: "",
-    video_url: "",
-    github: "",
+    image_url:
+      "https://drive.google.com/file/d/1sgjac9iPh3n6GuwBJGwsD0FXYFGRq_FH/view?usp=share_link",
+    video_url: "thhOujbmXdA",
+    github: "https://github.com/JoshS1234/pendulums-with-python",
     hosted: "",
     language: "MATLAB",
     tech: "",
@@ -84,10 +87,11 @@ module.exports = [
     year: 2020,
   },
   {
-    name: "Fractal pattern",
-    image_url: "",
+    name: "Mandelbrot fractal",
+    image_url:
+      "https://drive.google.com/file/d/1-SVGPFmJXTuKcGATV95k43eWoqjbGeYO/view?usp=share_link",
     video_url: "",
-    github: "",
+    github: "https://github.com/JoshS1234/fractals-in-python",
     hosted: "",
     language: "Python",
     tech: "",
@@ -96,9 +100,10 @@ module.exports = [
   },
   {
     name: "Handwriting recognition - Neural network",
-    image_url: "",
+    image_url:
+      "https://drive.google.com/file/d/1mKTWF0E9nZvFY8r5zaC9s2ngXv-i6ymv/view?usp=sharing",
     video_url: "",
-    github: "",
+    github: "https://github.com/JoshS1234/MNIST-number-recognition/tree/main",
     hosted: "",
     language: "Python",
     tech: "",
@@ -107,10 +112,10 @@ module.exports = [
     year: 2020,
   },
   {
-    name: "ASCII Noughts and Crosses",
-    image_url: "",
+    name: "ASCII Tic Tac Toe (Noughts and crosses),
+    image_url: "https://drive.google.com/file/d/1-SVGPFmJXTuKcGATV95k43eWoqjbGeYO/view?usp=share_link",
     video_url: "",
-    github: "",
+    github: "https://github.com/JoshS1234/TicTacToeAscii",
     hosted: "",
     language: "Python",
     tech: "",
@@ -120,9 +125,10 @@ module.exports = [
   },
   {
     name: "Blackjack",
-    image_url: "",
+    image_url:
+      "https://drive.google.com/file/d/1k0xDjl6bylxPU0wv00_qKh4Uoy2As4cT/view?usp=share_link",
     video_url: "",
-    github: "",
+    github: "https://github.com/JoshS1234/blackjackAscii",
     hosted: "",
     language: "Python",
     tech: "",
